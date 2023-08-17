e11363bf916cc17669e4d0e6acee4382f5101c25
dev-RowanSong <hyeho331@gmail.com>
2023-08-17 15:19:00 +0900

    [Init] init project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;400;600;700&display=swap');
+        </style>
+        <App />
+    </React.StrictMode>
+);
+
+reportWebVitals();
